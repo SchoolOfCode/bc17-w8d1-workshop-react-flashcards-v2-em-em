f81f79d67774d052db12af5b36bb8cdb69001fcc
Ryan <ryanshaunsmith@gmail.com>
2024-08-19 11:41:44 +0100

    added some content to the header and minor styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,10 @@ import styles from "./Header.module.css"
 
 export default function Header() {
     return (
-        <header className={styles.siteHeader}><img src={reactLogo}/></header>
+        <header className={styles.siteHeader}>
+            <img className={styles.logo} src={reactLogo}/>
+            <h1 className={styles.siteTitle}>React Flashcards</h1>
+            <p>🧠 Expand Your React Knowledge, One Flashcard at a Time! 🐌</p>    
+        </header>
     );
 }
