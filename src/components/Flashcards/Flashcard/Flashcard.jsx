93c4963090bf8b311a485bf30b485483d943259f
Abhiparth Shrestha <abhiparth@hotmail.co.uk>
2024-08-19 13:37:50 +0100

    made cards bigger and added content.

--- src/components/Flashcards/Flashcard/Flashcard.jsx
+++ src/components/Flashcards/Flashcard/Flashcard.jsx
@@ -3,6 +3,8 @@ import styles from './Flashcard.module.css';
 export default function Flashcard () {
     return (
         <div className = {styles.flashcard}>
+            <p className = {styles.emoji}>🤔</p>
+            <p className = {styles.text}>What is JSX?</p>
         </div>
     );
 }
